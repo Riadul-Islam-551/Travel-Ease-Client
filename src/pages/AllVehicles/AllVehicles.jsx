2f0df82a4abb452fe69f89f23f41b39c35d49f71
Riadul Islam <riadul551@gmail.com>
2025-11-14 13:04:10 +0600

    implement the sortiong method

--- src/pages/AllVehicles/AllVehicles.jsx
+++ src/pages/AllVehicles/AllVehicles.jsx
@@ -3,14 +3,102 @@ import VehecleCard from "../../components/VehecleCard/VehecleCard";
 
 const AllVehicles = () => {
   const [vehecles, setVehecles] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [locations, setLocations] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState("All");
+  const bangladeshDistricts = [
+    "Bagerhat",
+    "Bandarban",
+    "Barguna",
+    "Barisal",
+    "Bhola",
+    "Bogura",
+    "Brahmanbaria",
+    "Chandpur",
+    "Chapai Nawabganj",
+    "Chattogram",
+    "Chuadanga",
+    "Cox's Bazar",
+    "Cumilla",
+    "Dhaka",
+    "Dinajpur",
+    "Faridpur",
+    "Feni",
+    "Gaibandha",
+    "Gazipur",
+    "Gopalganj",
+    "Habiganj",
+    "Jamalpur",
+    "Jashore",
+    "Jhalokati",
+    "Jhenaidah",
+    "Joypurhat",
+    "Khagrachhari",
+    "Khulna",
+    "Kishoreganj",
+    "Kurigram",
+    "Kushtia",
+    "Lakshmipur",
+    "Lalmonirhat",
+    "Madaripur",
+    "Magura",
+    "Manikganj",
+    "Meherpur",
+    "Moulvibazar",
+    "Munshiganj",
+    "Mymensingh",
+    "Naogaon",
+    "Narail",
+    "Narayanganj",
+    "Narsingdi",
+    "Natore",
+    "Netrokona",
+    "Nilphamari",
+    "Noakhali",
+    "Pabna",
+    "Panchagarh",
+    "Patuakhali",
+    "Pirojpur",
+    "Rajbari",
+    "Rajshahi",
+    "Rangamati",
+    "Rangpur",
+    "Satkhira",
+    "Shariatpur",
+    "Sherpur",
+    "Sirajganj",
+    "Sunamganj",
+    "Sylhet",
+    "Tangail",
+    "Thakurgaon",
+  ];
 
   useEffect(() => {
     fetch("http://localhost:3000/vehicles")
       .then((res) => res.json())
       .then((data) => {
         setVehecles(data);
+        setFiltered(data);
+
+        const uniqueLocations = [...new Set(data.map((v) => v.location))];
+        setLocations(uniqueLocations);
       });
   }, []);
+
+  // Handle filtering by location
+  const handleFilter = (district) => {
+    setSelectedLocation(district);
+
+    if (district === "All") {
+      setFiltered(vehecles);
+    } else {
+      const filteredData = vehecles.filter(
+        (vehicle) => vehicle.location.toLowerCase() === district.toLowerCase()
+      );
+      setFiltered(filteredData);
+    }
+  };
+
   return (
     <div className="max-w-[1200px] mx-auto">
       <section className=" py-12">
@@ -25,10 +113,31 @@ const AllVehicles = () => {
           </p>
         </div>
       </section>
+      {/* filterd section  */}
+      
+      <section className="flex justify-between items-center m-6">
+        <section>
+          <p>Total : {filtered.length}</p>
+        </section>
+        <select
+          className="select select-bordered w-[250px] outline-none"
+          value={selectedLocation}
+          onChange={(e) => handleFilter(e.target.value)}
+        >
+          <option value="All" className="">All Districts</option>
+
+          {bangladeshDistricts.map((district, idx) => (
+            <option key={idx} value={district}>
+              {district}
+            </option>
+          ))}
+        </select>
+      </section>
+
       <section>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mx-auto my-12">
-          {vehecles.map((vehecle) => (
-            <VehecleCard key={vehecle._id} vehecle={vehecle} />
+          {filtered.map((vehicle) => (
+            <VehecleCard key={vehicle._id} vehecle={vehicle} />
           ))}
         </div>
       </section>
